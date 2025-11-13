8c2f80649b757690923cb7c6cc4eec9ea88b486d
seangg810-lab <seangg810@gmail.com>
2025-11-13 17:35:42 +0800

    change

--- TicTacToeTest.java
+++ TicTacToeTest.java
@@ -23,7 +23,7 @@ public class TicTacToeTest {
         game.set(1, 1); 
         game.set(0, 1); 
         game.set(0, 2); 
-        game.set(2, 1);
+        game.set(2, 0);
         game.set(2, 2); 
         assertTrue(game.isGameOver());
         assertEquals("O 勝利", game.getResult());
