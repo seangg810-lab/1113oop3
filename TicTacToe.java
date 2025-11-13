d31cd9e213f8ca9b8cb7f5347ae282d8b7305642
seangg810-lab <seangg810@gmail.com>
2025-11-13 17:32:38 +0800

    change

--- TicTacToe.java
+++ TicTacToe.java
@@ -70,9 +70,10 @@ public class TicTacToe {
         boolean full = true;
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                if (board[i][j] == ' ')
+                if (board[i][j] == ' '){
                     full = false;
                     break;
+                }
 
         if (full) {
             result = "平手";
